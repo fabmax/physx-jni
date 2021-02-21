f81549b0cbade4aba7334e522e6750311a3d8524
fabmax <fabmax.thiele@gmail.com>
2021-02-21 15:09:26 +0100

    more joint types

--- physx-jni/src/main/java/de/fabmax/physxjni/Loader.java
+++ physx-jni/src/main/java/de/fabmax/physxjni/Loader.java
@@ -12,7 +12,7 @@ import java.util.concurrent.atomic.AtomicBoolean;
 
 public class Loader {
 
-    private static final String version = "0.4.1";
+    private static final String version = "0.4.2-SNAPSHOT";
 
     private static final AtomicBoolean isLoaded = new AtomicBoolean(false);
 
