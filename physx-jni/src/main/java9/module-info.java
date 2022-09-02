5baf80fb178dfee7ee803c55c41407755fc9b03e
fabmax <fabmax.thiele@gmail.com>
2022-09-02 20:52:52 +0200

    added java9 module-info

---
+++ physx-jni/src/main/java9/module-info.java
@@ -0,0 +1,13 @@
+module de.fabmax.physxjni {
+    exports de.fabmax.physxjni;
+
+    exports physx;
+    exports physx.character;
+    exports physx.common;
+    exports physx.cooking;
+    exports physx.extensions;
+    exports physx.geomutils;
+    exports physx.physics;
+    exports physx.support;
+    exports physx.vehicle;
+}
---
+++ physx-jni/src/main/java9/physx/NonEmptyPackageDummy.java
@@ -0,0 +1,8 @@
+package physx;
+
+/**
+ * Apparently the multi-release plugin isn't smart enough to realize there are classes in this package in the default
+ * sources.
+ * Add an empty package-private dummy class to stop the compiler complaining about an empty package...
+ */
+class NonEmptyPackageDummy { }
