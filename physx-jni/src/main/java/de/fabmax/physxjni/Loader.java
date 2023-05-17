ee22280f3123dc7ab41824b52209430527d2c888
fabmax <fabmax.thiele@gmail.com>
2023-05-17 21:26:19 +0200

    added runtime platform check for cuda functions

--- physx-jni/src/main/java/de/fabmax/physxjni/Loader.java
+++ physx-jni/src/main/java/de/fabmax/physxjni/Loader.java
@@ -1,6 +1,7 @@
 package de.fabmax.physxjni;
 
 import physx.JniThreadManager;
+import physx.PlatformChecks;
 
 import java.util.concurrent.atomic.AtomicBoolean;
 
@@ -14,6 +15,19 @@ public class Loader {
         if (!isLoaded.getAndSet(true)) {
             Platform platform = Platform.getPlatform();
             try {
+                switch (platform) {
+                    case LINUX:
+                        PlatformChecks.setPlatformBit(PlatformChecks.PLATFORM_LINUX);
+                        break;
+                    case WINDOWS:
+                        PlatformChecks.setPlatformBit(PlatformChecks.PLATFORM_WINDOWS);
+                        break;
+                    case MACOS:
+                    case MACOS_ARM64:
+                        PlatformChecks.setPlatformBit(PlatformChecks.PLATFORM_MACOS);
+                        break;
+                }
+
                 NativeLib lib = platform.getLib();
                 if (!version.equals(lib.getVersion())) {
                     throw new IllegalStateException("Native lib version " + lib.getVersion() +
